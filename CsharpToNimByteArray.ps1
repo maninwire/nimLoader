a9d350df42d46baa96e3b1ae09f8bef51b75bae8
maninwire <31477337+maninwire@users.noreply.github.com>
2021-12-23 18:09:03 +0100

    Add files via upload
    Initial push

---
+++ CsharpToNimByteArray.ps1
@@ -0,0 +1,55 @@
+function CSharpToNimByteArray
+{
+
+Param
+    (
+        [string]
+        $inputfile,
+	    [switch]
+        $folder
+)
+
+    if ($folder)
+    {
+        $Files = Get-Childitem -Path $inputfile -File
+        $fullname = $Files.FullName
+        foreach($file in $fullname)
+        {
+            Write-Host "Converting $file"
+            $outfile = $File + "NimByteArray.txt"
+    
+            [byte[]] $hex = get-content -encoding byte -path $File
+            $hexString = ($hex|ForEach-Object ToString X2) -join ',0x'
+            #$Results = $hexString.Insert(0,"var buf: array[" + $hex.Length + ", byte] = [byte 0x")
+            #$Results = $Results + "]"         
+            #$Results | out-file $outfile
+            $hexString="0x"+$hexString
+            $hexString| out-file $outfile -encoding utf8
+         
+        }
+        Write-Host -ForegroundColor yellow "Results Written to the same folder"
+    }
+    else
+    {
+        Write-Host "Converting $inputfile"
+        $outfile = $inputfile + "NimByteArray.txt"
+        
+        [byte[]] $hex = get-content -encoding byte -path $inputfile
+        $hexString = ($hex|ForEach-Object ToString X2) -join ',0x'
+        #$Results = $hexString.Insert(0,"var buf: array[" + $hex.Length + ", byte] = [byte 0x")
+        #$Results = $Results + "]" 
+
+        #write-output $Results | out-file $outfile
+        $hexString="0x"+$hexString
+        $hexString| out-file $outfile -encoding utf8        
+        Write-Host "Result Written to $outfile"
+
+    }
+
+
+        # dos2unix conversion
+
+        [string]::Join("`n",(gc $outfile)) | sc $outfile
+
+}
+        Write-Host "Done"
---
+++ nimCrypter.nim
@@ -0,0 +1,92 @@
+import nimcrypto
+import nimcrypto/sysrand
+import base64
+import os   
+import strformat
+import strutils
+import std/parseopt # option parser
+
+
+const iv: array[aes256.sizeBlock, byte]= [byte 55, 19, 19, 173, 190, 70, 130, 254, 26, 241, 14, 4, 213, 94, 108, 237]
+const envkey: string = "TARGETDOMAIN"
+
+
+
+func toByteSeq*(str: string): seq[byte] {.inline.} =
+    # Converts a string to the corresponding byte sequence
+    @(str.toOpenArrayByte(0, str.high))
+
+
+when isMainModule:
+
+
+    var inFile: string =""
+    var operation: string = ""
+    var passkeystr:string=envkey# default key to our constant
+    var helpMsg: string="nimEncrypter.exe [-d] [-e] [-k] file.txt" # help msg
+    var debug: bool=false # to enable/disable debug mode
+
+
+    # OUR OPTION PARSER
+    var parser = initOptParser() 
+    while true:
+      parser.next()
+      case parser.kind
+      of cmdEnd: 
+        break
+      of cmdShortOption, cmdLongOption:
+        if parser.val == "": #just options "-x"
+          if parser.key=="d": # decrypt option
+            operation="decrypt"
+          if parser.key=="e": # encrypt option
+            operation="encrypt"
+          elif parser.key=="D": # debug option
+            debug=true
+          elif parser.key=="h": # help option
+            echo helpMsg
+            quit(QuitSuccess)
+
+        else: # have value
+
+          if parser.key=="k": # key option
+            passkeystr=parser.val
+
+      of cmdArgument:
+        inFile=parser.key # argument input file
+
+    if inFile=="" or operation=="":
+      quit()
+
+
+
+    
+    let inFileContents: string = readFile(inFile)
+    var 
+
+        data: seq[byte] = toByteSeq(inFileContents)
+        ectx: CTR[aes256]
+        key: array[aes256.sizeKey, byte]
+        plaintext = newSeq[byte](len(data))
+        transformedText = newSeq[byte](len(data))
+
+
+    copyMem(addr plaintext[0], addr data[0], len(data))
+    # Expand key to 32 bytes using SHA256 as the KDF
+    var expandedkey = sha256.digest(passkeystr)
+    copyMem(addr key[0], addr expandedkey.data[0], len(expandedkey.data))
+
+
+    ectx.init(key, iv)
+    if operation=="encrypt":
+        ectx.encrypt(plaintext, transformedText)
+        writeFile(inFile&"_enc.txt", transformedText)
+
+    elif operation=="decrypt":
+        ectx.decrypt(plaintext, transformedText)
+        writeFile(inFile&"_dec.txt", transformedText)     
+    else:
+        ectx.clear()
+        system.quit()
+
+
+        
